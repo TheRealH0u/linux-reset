ebde3580c9f0add7fccaa4bf2b2e141d2b36d36d
TheRealH0u <aplazl55@gmail.com>
2022-03-03 09:34:57 +0100

    Added directo writing to README with TOC

--- fromDataToREADME.py
+++ fromDataToREADME.py
@@ -1,12 +1,37 @@
 import json
+import os
+
+README = ""
+tableOfContents = ""
 
 with open('website/json/data.json') as f:
     data = json.load(f)
+
+    README += "# linux-reset\n"
+    README += "Repository to download important files for a new linux system.\n"
+
+    tableOfContents += "- [linux-reset](#linux-reset)\n"
+
     for tab in data:
-        print("## "+tab.capitalize())
+        
+        README += "## "+tab.capitalize()+"\n"
+        
+        tableOfContents += "  - ["+tab.capitalize()+"](#"+tab+")\n"
+        
         for category in data[tab]:
-            print("### "+category["name"])
-            print("| | |")
-            print("|-|-|")
+            
+            README += "### "+category["name"]+"\n"
+            README += "| | |\n"
+            README += "|-|-|\n"
+
+            tableOfContents += "    - ["+category["name"]+"](#" + category["name"].lower()+")\n"
+
             for program in category["programs"]:
-                print("|"+program["name"]+"|"+program["description"]+"|")
+                README += "|"+program["name"]+"|"+program["description"]+"|\n"
+try:
+    os.system("touch README.md")
+except:
+    pass
+
+with open("README.md", "w") as f:
+    f.write(tableOfContents+"\n"+README)
