382c3f30c6407334f2026b5eebe8ddca83f92a84
TheRealH0u <aplazl55@gmail.com>
2022-03-03 02:47:16 +0100

    Uff did a lot so idk

--- website/js/main.js
+++ website/js/main.js
@@ -1,32 +1,38 @@
-$(document).ready(function() {
-    console.log("Page ready!");
-    $.getJSON('json/data.json', function(data) {
-        $.each(data, function(itemName, item) {
-            $.each(item, function(i, category) {
-                $("#v-pills-" + itemName).append('<hr style="margin: 5px 0"><span style="margin-bottom: 10px" class="badge bg-primary">' + category.name + '</span>');
-                $.each(category.programs, function(i, v) {
-                    $("#v-pills-" + itemName).append(
-                        '<div class="form-check form-switch form-secondary"><input class="form-check-input ' + itemName + '" type="checkbox" name="' + itemName + '" id="' + v.id + '"><label class="form-check-label" for="' + v.id + '">' + v.name + '</label></div>'
-                    );
-                    $("#" + v.id).data("install", v.install);
+var $j = jQuery.noConflict();
+
+$j(document).ready(function() {
+    $j.getJSON('json/data.json', function(data) {
+        $j.each(data, function(itemName, item) {
+            $j.each(item, function(i, category) {
+                $j("#v-pills-" + itemName).append('<hr style="margin: 5px 0"><span style="margin-bottom: 10px" class="badge bg-primary">' + category.name + '</span>');
+                $j.each(category.programs, function(i, v) {
+                    $j("#v-pills-" + itemName).append(
+                        '<div class="form-check form-switch form-secondary"><input class="form-check-input ' + itemName + '" type="checkbox" name="' + itemName + '" id="' + v.id + '"><label class="form-check-label" for="' + v.id + '">' + v.name + '</label>&nbsp;<label tabindex="0" class="far fa-question-circle" role="button" data-bs-toggle="popover" data-bs-trigger="focus" title="' + v.name + '" data-bs-content="' + v.description + '"></label></div>'
+                    ); //data-toogle="tooltip" title="awd" data-bs-trigger="focus"
+                    $j("#" + v.id).data("install", v.install);
                 });
+                var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+                popoverTriggerList.map(function(popoverTriggerEl) {
+                    return new bootstrap.Popover(popoverTriggerEl, { trigger: 'focus' })
+                })
+                console.log("loaded");
             });
         });
     });
 });
 
-$(document).on('click', '.programs, .services, .tools, .miscs', function(e) {
-    if ($(this).is(":checked")) {
-        $("#script").text($("#script").text() + "\n" + $(this).data("install"));
+$j(document).on('click', '.programs, .services, .tools, .miscs', function(e) {
+    if ($j(this).is(":checked")) {
+        $j("#script").text($j("#script").text() + "\n" + $j(this).data("install"));
     } else {
-        $("#script").text($("#script").text().replace("\n" + $(this).data("install"), ""));
+        $j("#script").text($j("#script").text().replace("\n" + $j(this).data("install"), ""));
     }
     document.querySelector("#script").style.height = "1px";
     document.querySelector("#script").style.height = (25 + document.querySelector("#script").scrollHeight) + "px";
 });
 
-$(document).on('focusout', '.dir', function() {
-    $("#script").text($("#script").text().replace(/\$dir=(.*)/g, "$dir=/home/" + $(".dir").val()));
+$j(document).on('focusout', '.dir', function() {
+    $j("#script").text($j("#script").text().replace(/\$jdir=(.*)/g, "$jdir=/home/" + $j(".dir").val()));
 })
 
 function textAreaAdjust(element) {
@@ -35,21 +41,21 @@ function textAreaAdjust(element) {
 }
 
 function upgrade(e, name) {
-    if ($(e).is(":checked")) {
-        $("#script").text($("#script").text().replace("#" + name, "sudo apt " + name + " -y"));
+    if ($j(e).is(":checked")) {
+        $j("#script").text($j("#script").text().replace("#" + name, "sudo apt " + name + " -y"));
     } else {
-        $("#script").text($("#script").text().replace("sudo apt " + name + " -y", "#" + name));
+        $j("#script").text($j("#script").text().replace("sudo apt " + name + " -y", "#" + name));
     }
 }
 
 function copy(e) {
-    $("#script").select();
+    $j("#script").select();
     document.execCommand("copy");
 }
 
 function download(e) {
     var downloadableLink = document.createElement('a');
-    downloadableLink.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent($("#script").val()));
+    downloadableLink.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent($j("#script").val()));
     downloadableLink.download = "update.sh";
     document.body.appendChild(downloadableLink);
     downloadableLink.click();
